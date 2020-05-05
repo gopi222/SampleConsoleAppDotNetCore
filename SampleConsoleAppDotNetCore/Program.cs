e65dca53e28611c0e8da6f969c3d7db3c25190d9
Gopinath S <gopi222@gmail.com>
2020-05-05 19:30:40 +0530

    first commit

--- SampleConsoleAppDotNetCore/Program.cs
+++ SampleConsoleAppDotNetCore/Program.cs
@@ -6,7 +6,7 @@ namespace SampleConsoleAppDotNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Gopinath!");
         }
     }
 }
